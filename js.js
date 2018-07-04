7c0ee2b6b84d7ffd94723013ac65f75b8c5dc07e
wx04 <690241319>
2018-07-04 18:47:20 +0800

    first

---
+++ js.js
@@ -0,0 +1,10 @@
+/*
+
+var wrap=document.getElementsByTagName("section");
+var i=0
+  wrap[i].onmousemove=function () {
+    wrap[i].className="xwarp";
+      wrap[i].childNodes.style.color="#FFFFFF";
+
+};
+*/
